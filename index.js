b83ed1261da3377b3a0aeeea6fab385fb2cdfcff
Junho-Kim <kjh44004@naver.com>
2016-06-14 14:43:40 +0900

    Make Board
    Make Board

---
+++ index.js
@@ -0,0 +1,5 @@
+function popupOpen(){
+	var popUrl = "http://naver.com";	//팝업창에 출력될 페이지 URL
+	var popOption = "width=370, height=360, resizable=no, scrollbars=no, status=no;";    //팝업창 옵션(optoin)
+	window.open(popUrl,"",popOption);
+}
